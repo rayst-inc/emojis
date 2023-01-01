e282bd949331863076048b00e62dfca8e32edd5c
rayst <a@ray.st>
2023-01-01 14:23:58 +0900

    Add CF analytics

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /**
  * @type {import('next').NextConfig}
  */
-module.exports = {}
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/cf',
+        destination: 'https://static.cloudflareinsights.com/beacon.min.js',
+        basePath: false,
+      },
+      {
+        source: '/cf/:path*',
+        destination: 'https://cloudflareinsights.com/:path*',
+        basePath: false,
+      },
+    ]
+  },
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from '@vercel/analytics/react'
+import { CloudflareAnalytics } from 'components/CloudflareAnalytics'
 import { NextSeo } from 'next-seo'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
@@ -29,6 +30,7 @@ export default function App({ Component, pageProps }: AppProps) {
             src="https://u.ray.st/u.js"
           ></Script>
           <Analytics />
+          <CloudflareAnalytics token="65fb0496c44b4112bcece211f908d428" />
         </>
       )}
     </>
